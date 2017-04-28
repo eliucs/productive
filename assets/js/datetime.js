909a1dc7656e8bf2dabc7cf3bbd8f86642c4362e
Eric Liu <ericliu314@gmail.com>
2017-04-28 00:43:30 -0400

    Renames time to datetime

--- assets/js/datetime.js
+++ assets/js/datetime.js
@@ -1,27 +1,50 @@
-function getDateText() {
-    var monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"];
-    var date = new Date();
-    var month = date.getMonth();
-    var day = date.getDate();
-    var year = date.getFullYear();
-    return monthNames[month] + " " + day + ", " + year;
-}
+$(document).ready(function() {
 
-/*
-function startTime() {
-    var time = new Date();
-    var hour = time.getHours();
-    var min = time.getMinutes();
-
-    if (min < 10) {
-        min = "0" + min;
+    /**
+     * timeAppendZero appends a '0' to the number if it
+     * is less than 10
+     *
+     * @param {Number} i - The number
+     * @returns {String} The number with a '0' appended to the front
+     */
+    function timeAppendZero(i) {
+        if (i < 10) { i = "0" + i }
+        return i;
     }
 
-    document.getElementById('time').innerHTML = h + ":" + m;
-    t = setTimeout(function () {
-        startTime()
-    }, 500);
-}
+    /**
+     * updateTime changes the time div element to the current time
+     *
+     */
+    function updateTime() {
+        var time = new Date();
+        var hour = time.getHours();
+        var min = time.getMinutes();
+        hour = timeAppendZero(hour);
+        min = timeAppendZero(min);
+        $("#time").html(hour + ":" + min);
+        time = setTimeout(function () {
+            updateTime()
+        }, 500);
+    }
+    updateTime();
 
-startTime();*/
+    /**
+     * updateDate changes the date div element to the current date
+     *
+     * @returns {string} The date formatted in Month Day, Year format
+     */
+    function updateDate() {
+        var monthNames = ["January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"];
+        var date = new Date();
+        var month = date.getMonth();
+        var day = date.getDate();
+        var year = date.getFullYear();
+        return monthNames[month] + " " + day + ", " + year;
+    }
+    $("#date").html(updateDate());
+
+});
+
+
